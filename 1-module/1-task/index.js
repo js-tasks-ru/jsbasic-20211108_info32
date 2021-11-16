0b9543b8b509483154f65f8dff12101b98278b08
GandraburGhenadii <ghenadii@kenwood.md>
2021-11-16 21:20:46 +0200

    Проверка npm test

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,13 +1,14 @@
-//ввод параметра
-let n = prompt("Enter the number",5);
-//если не сработает цикл (n=1)
-let factorialNumber = 1;
+
 function factorial(n) {
-  for (n ; n >1; n--){
-    factorialNumber *=n;
- }
-  return factorialNumber;
+  let factorialNumber = 1;
+  if (n == 0 || n == 1) {
+    return factorialNumber;
+  } else {
+    for ( n; n >= 1; n--) {
+      factorialNumber += n;
+    }
+    return factorialNumber;
+  }
 }
 
-  document.getElementById("fac").innerHTML = 'Factorial of ' + n + ' is -' + factorial(n);
-//Для проверки открыть hw.html
+
